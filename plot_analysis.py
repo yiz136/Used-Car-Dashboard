d65b3d79bb8d4ff856b9cce4079fa5df281b725d
Yifei <yifeiz@Yifeis-MacBook-Pro.local>
2022-06-01 22:04:30 -0700

    latest changes

--- plot_analysis.py
+++ plot_analysis.py
@@ -1,31 +1,37 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
+from scipy import stats
 
-def preprocess(fname, drops=['condition', 'size']):
-    '''
-    Preprocesses the dataframe by converting posting date to datetime
-    and adding ranges to price/year.
-    :return:
-    '''
+def preprocess(fname='vehicles_cleaned_imputed.csv', drops=['condition', 'VIN']):
+    """Preprocesses the data from the csv in fname.
+
+    For the csv provided, the function will drop irrelevant columns,
+    correct or convert the values in a few of the relevant columns,
+    and add categorical equivalents (grouped by four ranges) to the numeric columns.
+
+    Args:
+        fname (str): The filename of the csv file to be preprocessed.
+        drops (list): The columns to be dropped.
+    Returns:
+        pd.DataFrame: The preprocessed data.
+    """
     df = pd.read_csv(fname, header=0, index_col=0)
 
     all_drops = ['url', 'region_url'] + drops
     df.drop(all_drops, inplace=True, axis=1)
-    print(len(df['manufacturer'].unique()))
 
     # Correction of previous columns
     df['state'] = df['state'].str.upper()
-    print('state')
-    # df['posting_date'] = pd.to_datetime(df['posting_date'], format='%Y-%m-%dT%H:%M:%S', cache=True)
-    df['posting_date'] = pd.to_datetime(df['posting_date'], infer_datetime_format=True, cache=True)
-    print('date')
     df['price'] = df['price'].replace(0, np.nan)
-    print('price')
     df = df.dropna(subset=['price'])
+    df = df[df['price'] < 300000]
 
-    print(len(df['price'].dropna()), len(df['price']))
+    # Split posting date to year and month
+    df['posting_date'] = pd.to_datetime(df['posting_date'], utc=True, infer_datetime_format=True, cache=True)
+    df['posting_year'] = pd.DatetimeIndex(df['posting_date']).year
+    df['posting_month'] = pd.DatetimeIndex(df['posting_date']).month
+
     df['price_range'] = pd.cut(df['price'],
                                [-np.inf, 5000, 15000, 25000, np.inf],
                                labels=['<$5000', '$5000-15000', '$15000-25000', '>$25000']
@@ -38,49 +44,36 @@ def preprocess(fname, drops=['condition', 'size']):
                               [-np.inf, 40000, 80000, 120000, np.inf],
                               labels=['<40000', '40000-80000', '80000-120000', '>120000']
                               )
-    df.to_csv('data/processed.csv')
     return df
 
 def plot_pie(col, lim = 15):
-    '''
-    Plots the pie chart of the categorical variable col.
-    :param col: The name of the categorical column.
-    :param lim: The top number of values to group by.
-    :return:
-    '''
+    """Plots the pie chart of the categorical variable col.
+
+    The col variable must be the name of a categorical column of the dataframe.
+    From there, it will create a pie chart of the most frequent values up to lim.
+    All other values will be assigned to the 'Other' group.
+
+    Args:
+        col (str): The name of the categorical column.
+        lim (int): The top number of values to group by.
+    """
     assert col in cats
 
     values = df[col].dropna().value_counts().sort_values(ascending=False)
     topk = min(len(values), lim)
     top_values = values.nlargest(topk)
-    if topk > lim: top_values['other'] = values.nsmallest(values.size - topk).sum()
+    if len(values) > lim: top_values['other'] = values.nsmallest(values.size - topk).sum()
     title = 'Number of cars by %s' % (col.replace('_', ' '))
     fig = px.pie(values, values=top_values.values, names=top_values.index, title=title)
 
     fig.show()
 
-def plot_two(col1, col2): pass
-    # assert col1 in df.columns and col2 in df.columns
-    # df_nonull = df.dropna(subset=[col2], axis='index')
-    # p = df_nonull.groupby([col1]).mean()[col2]
-    # statewise_prices = pd.DataFrame({'states': p.index, 'avg_price': p.values})
-    # statewise_prices['states'] = statewise_prices['states'].str.upper()
-    #
-    # # TODO - split into groups of price values or percentiles to distinguish easily
-    # fig = px.choropleth(statewise_prices, locations='states', locationmode="USA-states",
-    #                     color='avg_price',
-    #                     # color_continuous_scale="Viridis",
-    #                     scope="usa",
-    #                     color_continuous_scale=px.colors.sequential.Plasma
-    #                     )
-    # fig.show()
-
 def get_groupby_columns(col1, col2='url', agg='count', num_vals=1):
-    """ This function returns col2 grouped by col1 with aggregation function applied
+    """ This function returns col2 grouped by col1 with the aggregation function agg.
 
     Args:
-        col1 (str): column to groupby
-        col2 (str, optional): column to return. Defaults to 'url'
+        col1 (str): The column to group by
+        col2 (str, optional): The column to return. Defaults to 'url'.
         agg (str, optional): Aggregation function to be applied. Defaults to 'count'.
         num_vals (int, optional): Top n categories of col1 are returned to make plotting easier. Defaults to 1.
     Returns:
@@ -103,6 +96,6 @@ def get_groupby_columns(col1, col2='url', agg='count', num_vals=1):
     # TODO: add topk handling
 
 if __name__ == '__main__':
-    df = preprocess('data/vehicles_no_url_des_filled.csv')
+    df = preprocess()
     cats, nums = df.select_dtypes(['object', 'category']).columns, df.select_dtypes(np.number).columns
-    plot_pie('price_range')
+    plot_pie('manufacturer')
